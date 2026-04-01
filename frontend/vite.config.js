461b6387108490a1db1a661061f771d552a7cc38
TKAman23 <karthikeyaat@gmail.com>
2026-03-31 23:23:56 -0700

    attempt to publish to github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // Base path for GitHub/Firebase hosting — keep as '/' for Firebase Hosting
-  base: '/',
-})
+  base: "/Logement-Intergenerationnel/",
+});
